84e6a4d3af4dc30a23d9200829a9583e28d52822
HIENLTH @ HCMUE <hyhien@gmail.com>
2025-03-18 08:45:28 +0700

    useParams demo

--- Buoi04/demorouter/src/App.jsx
+++ Buoi04/demorouter/src/App.jsx
@@ -1,23 +1,29 @@
-import { Routes, Route, Link} from 'react-router-dom'
+// import { Routes, Route, Link} from 'react-router-dom'
+import { Link, Routes, Route } from 'react-router'
 import { Home } from './components/Home'
 import { Product } from './components/Product'
-import { Profile, ChangePassword } from './components/Profile'
+import { Profile, ChangePassword, Users, User } from './components/Profile'
 import { NoMatch } from './components/NoMatch'
 function App() {
-
+  const users = [
+    { id: '1', fullName: 'Lê Li La' },
+    { id: '2', fullName: 'Hoàng Lê' },
+  ];
   return (
     <>
-      <div>
+      <nav>
         <Link to="/">Home</Link> | 
         <Link to="/profile">Profile</Link> | 
         <Link to="/products">Product</Link>
-      </div>
+      </nav>
       <Routes>
         <Route path='/' element={ <Home /> } />
-        <Route path='/profile' element={ <Profile /> }>
-          <Route path='/profile/changepassword' element={<ChangePassword /> } />
+        <Route path='profile' element={ <Profile /> }>
+          <Route path='changepassword' element={<ChangePassword /> } />
         </Route>
         <Route path='/products' element={ <Product /> } />
+        <Route path="users" element={<Users users={users} />} />
+        <Route path="users/:userId" element={<User />} />
         <Route path='*' element={<NoMatch/> } />
       </Routes>
     </>
--- Buoi04/demorouter/src/components/Profile.jsx
+++ Buoi04/demorouter/src/components/Profile.jsx
@@ -1,3 +1,5 @@
+import {useParams, Link} from 'react-router'
+
 export const Profile =() => {
     return (
         <div>
@@ -17,3 +19,28 @@ export const ChangePassword =() => {
         </div>
     )
 }
+
+export const Users = ({ users }) => {
+  return (
+    <>
+      <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={user.id}>{user.fullName}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export const User = () => {
+  const { userId } = useParams();
+  return (
+    <>
+      <h2>User: {userId}</h2>
+      <Link to="/users">Back to Users</Link>
+    </>
+  );
+};
