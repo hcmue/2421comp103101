092959eb881f8bee472b583bb166acf3872540bf
HIENLTH @ HCMUE <hyhien@gmail.com>
2025-03-18 09:06:00 +0700

    Get product list

--- Buoi04/README.md
+++ Buoi04/README.md
@@ -12,3 +12,9 @@
 - Dựng lại thư viện: `npm i`
 
 - Cài thư viện react router: `npm i react-router-dom`
+
+## Về nhà
+
+Thử liếc qua trang https://fakeapi.platzi.com/ hoặc các API khác để gọi API CRUD
+
+- Thử tìm product theo các tiêu chí, tham khảo https://fakeapi.platzi.com/en/rest/products-filter/
--- Buoi04/demorouter/src/components/Product.jsx
+++ Buoi04/demorouter/src/components/Product.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react"
+
 export const Product =() => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("https://api.escuelajs.co/api/v1/products")
+            .then(response => response.json())
+            .then(json => setProducts(json))
+    }, []);
     return (
         <div>
             <h2>PRODUCT PAGE</h2>
-            <div>Total .... product(s).</div>
+            <div>Total {products.length} product(s).</div>
+            {products.map((item, idx)=> {
+                // console.log(item)
+                return (
+                    <div key={idx} class="grid grid-5 not-content">
+                        <article>
+                            <img loading="lazy" src={item.images[0]} alt={item.title} style={{width: 100, height: 120}} /> <p>${item.price}</p>
+                            <h4>{item.title}</h4> 
+                        </article>
+                    </div>
+                )
+            }
+            )}
         </div>
     )
 }
